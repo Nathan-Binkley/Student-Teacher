27237f9bcf867ec9113a5a9494d4545df4847ac3
Nathan-Binkley <nbink2@gmail.com>
2020-08-09 15:46:46 -0400

    now runs faster after first run

--- parseData.py
+++ parseData.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 special_converted_files = ['2018Fall.csv','2018Spring.csv'] #these files have special formatting for names (ie ' "Smith, John William" ' is normal and in these it's simply 'Smith John William' without commas or quotes )
 #TBH, this is the easier way to parse but whatevs
@@ -126,73 +127,84 @@ def getInitials(Name):
     return initials
 
 def initialize():
-    with open("master.csv", "r") as f:
-        line = f.readline()
-        while(line):
-            fileLines.append(line)
+    global master_list, master_prof_list
+
+    if os.path.isfile('./master.json'): #SKIP PARSE DATA WHEN NOT NECESSARY
+        print("FOUND COURSE FILE")
+        with open('master.json', 'r') as f:
+            master_list = json.load(f)
+        # print(master_list)
+        if os.path.isfile('./master_prof.json'):
+            print("FOUND PROF FILE")
+            with open('master_prof.json', 'r') as f:
+                master_prof_list = json.load(f)
+        else:
+            print("PROF FILE NOT FOUND")
+    else:
+    
+        with open("master.csv", "r") as f:
             line = f.readline()
-        #print("End of file. Length of fileLines = " + str(len(fileLines)))
+            while(line):
+                fileLines.append(line)
+                line = f.readline()
+            #print("End of file. Length of fileLines = " + str(len(fileLines)))
 
-        for i in fileLines:
-            
-            testData = i.split(",")
-            course = testData[0]
-            number = testData[1]
-            section = testData[2]
-            course_title = testData[3]
-            A_Grade = testData[4]
-            B_Grade = testData[5]
-            C_Grade = testData[6]
-            D_Grade = testData[7]
-            F_Grade = testData[8]
-            P_Grade = testData[9]
-            F_P_Grade = testData[10]
-            Withdraw = testData[11]
-            if fileLines[-1] == "H":
-                Name = "".join(testData[12:-1])
-                Honors = testData[-1]
-            else:
-                Name = "".join(testData[12:])
-            Name = Name.strip()
+            for i in fileLines:
+                testData = i.split(",")
+                course = testData[0]
+                number = testData[1]
+                section = testData[2]
+                course_title = testData[3]
+                A_Grade = testData[4]
+                B_Grade = testData[5]
+                C_Grade = testData[6]
+                D_Grade = testData[7]
+                F_Grade = testData[8]
+                P_Grade = testData[9]
+                F_P_Grade = testData[10]
+                Withdraw = testData[11]
+                if fileLines[-1] == "H":
+                    Name = "".join(testData[12:-1])
+                    Honors = testData[-1]
+                else:
+                    Name = "".join(testData[12:])
+                Name = Name.strip()
 
-            if Name[0] == '"':
-                Name = Name[1:-1]
+                if Name[0] == '"':
+                    Name = Name[1:-1]
 
-            FL = getFirstLast(Name)
+                FL = getFirstLast(Name)
 
 
-            data = {
-                "name":course_title,
-                "A":A_Grade,
-                "B":B_Grade,
-                "C":C_Grade,
-                "D":D_Grade,
-                "F":F_Grade,
-                "W":Withdraw,
-                "Professor": Name,
-                "Prof Initials": getInitials(Name)
-            }
+                data = {
+                    "name":course_title,
+                    "A":A_Grade,
+                    "B":B_Grade,
+                    "C":C_Grade,
+                    "D":D_Grade,
+                    "F":F_Grade,
+                    "W":Withdraw,
+                    "Professor": Name,
+                    "Prof Initials": getInitials(Name)
+                }
 
-            if course+"-"+number in master_list:
-                master_list[course+"-"+number].append(data)
-            else:
-                master_list[course+"-"+number] = []
-                master_list[course+"-"+number].append(data)
+                if course+"-"+number in master_list:
+                    master_list[course+"-"+number].append(data)
+                else:
+                    master_list[course+"-"+number] = []
+                    master_list[course+"-"+number].append(data)
 
-            if FL in master_prof_list:
-                master_prof_list[FL].append(data)
-            else:
-                master_prof_list[FL] = []
-                master_prof_list[FL].append(data)
-            
-    # with open("master_ind.json","w+") as f: #Primarily used for debugging atm
-    #     json.dump(master_list, f, indent=4)
-    # with open("master_prof_ind.json", "w+") as f:
-    #     json.dump(master_prof_list, f, indent=4)
-    with open("master.json","w+") as f: #Unformatted data, useful for space
-        json.dump(master_list, f)
-    with open("master_prof.json", "w+") as f:
-        json.dump(master_prof_list, f)
+                if FL in master_prof_list:
+                    master_prof_list[FL].append(data)
+                else:
+                    master_prof_list[FL] = []
+                    master_prof_list[FL].append(data)
+                
+        with open("master.json","w+") as f: #Unformatted data, useful for space
+            json.dump(master_list, f)
+        with open("master_prof.json", "w+") as f:
+            json.dump(master_prof_list, f)
+          
 
 def getFirstLast(Name):
     FML = Name.split()
