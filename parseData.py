37586eb5b0d177ee39d3c4d3f870787d32c6a88c
Nathan-Binkley <nbink2@gmail.com>
2020-08-10 16:42:46 -0400

    updated parse data to remove vulnerability + add exceptions

--- parseData.py
+++ parseData.py
@@ -18,103 +18,106 @@ def process_Search(orig_query: str) -> str: #Primary search function and process
     items = query.split("-")
     master_String = ''
 
-    if("ALL" in items):
-        print("ALL IN QUERY")
-        print("HERE COMES THE SUN")
-        if items[0] == "ALL" and items[1] == "ALL":
-            print("ALLALL")
-            for i in master_list:
-                master_String += process_Search(i)
-        elif items[0] == "ALL":
-            for i in master_list:
-                if items[1] in i:
-                    master_String += process_Search(i)
-        elif items[1] == "ALL":
-            for i in master_list:
-                if items[0] in i:
-                    master_String += process_Search(i)
-        else:
-            print("Misunderstood")
-            return "HELP"
-        return master_String
+    # if("ALL" in items):
+    #     print("ALL IN QUERY")
+    #     print("HERE COMES THE SUN")
+    #     if items[0] == "ALL" and items[1] == "ALL":
+    #         print("ALLALL")
+    #         for i in master_list:
+    #             master_String += process_Search(i)
+    #     elif items[0] == "ALL":
+    #         for i in master_list:
+    #             if items[1] in i:
+    #                 master_String += process_Search(i)
+    #     elif items[1] == "ALL":
+    #         for i in master_list:
+    #             if items[0] in i:
+    #                 master_String += process_Search(i)
+    #     else:
+    #         print("Misunderstood")
+    #         return "HELP"
+    #     return master_String
 
-    else:
+    # else:
+    if query in master_list:
         data_list = master_list[query]
-        name = data_list[-1]['name'] # Get the most recent course name on file. If we did the beginning, it'd throw the incorrect name because Clemson is big dumb.
-        # print(data_list)
-        A = []
-        B = []
-        C = []
-        D = []
-        F = []
-        W = []
-        P = []
-        NP = []
-        
+    else:
+        raise NotADirectoryError
+    name = data_list[-1]['name'] # Get the most recent course name on file. If we did the beginning, it'd throw the incorrect name because Clemson is big dumb.
+    # print(data_list)
+    A = []
+    B = []
+    C = []
+    D = []
+    F = []
+    W = []
+    P = []
+    NP = []
     
-        professorGrades = {}
+
+    professorGrades = {}
+
+    
+    for i in data_list:
+        A.append(i['A'])
+        B.append(i['B'])
+        C.append(i['C'])
+        D.append(i['D'])
+        F.append(i['F'])
+        W.append(i['W'])
 
         
-        for i in data_list:
-            A.append(i['A'])
-            B.append(i['B'])
-            C.append(i['C'])
-            D.append(i['D'])
-            F.append(i['F'])
-            W.append(i['W'])
+        searchableName = getFirstLast(i['Professor'])
 
-            
-            searchableName = getFirstLast(i['Professor'])
-
-            professorGrades[searchableName] = []
-            
-
-        gradesList = (A,B,C,D,F,W,P,NP)
-        
-        for letterGrade in gradesList:
-            for i in range(len(letterGrade)):
-                letterGrade[i] = int(letterGrade[i][:-1])
-
-        AvgA = sum(A)//len(A)
-        AvgB = sum(B)//len(B)
-        AvgC = sum(C)//len(C)
-        AvgD = sum(D)//len(D)
-        AvgF = sum(F)//len(F)
-        AvgWithdraw = sum(W)//len(W)
-
-        courseString = "Average; A: " + str(AvgA) + "% B: " + str(AvgB) + "% C: " + str(AvgC) + "% D: " + str(AvgD) + "% F: " + str(AvgF) + "% W: " + str(AvgWithdraw) + "% from " + str(len(data_list)) + " class(es) for " + orig_query + ": " + name
+        professorGrades[searchableName] = []
         
 
-        bestProfName = "" #Professor name to go in professor string
-        bestProfAB = 0
-        bestProfLenCount = 0
+    gradesList = (A,B,C,D,F,W,P,NP)
+    
+    for letterGrade in gradesList:
+        for i in range(len(letterGrade)):
+            letterGrade[i] = int(letterGrade[i][:-1])
 
-        worstProfName = ""
-        worstProfFW = 0
-        worstProfLenCount = 0
+    AvgA = sum(A)//len(A)
+    AvgB = sum(B)//len(B)
+    AvgC = sum(C)//len(C)
+    AvgD = sum(D)//len(D)
+    AvgF = sum(F)//len(F)
+    AvgWithdraw = sum(W)//len(W)
 
-        data = []
+    courseString = "Average; A: " + str(AvgA) + "% B: " + str(AvgB) + "% C: " + str(AvgC) + "% D: " + str(AvgD) + "% F: " + str(AvgF) + "% W: " + str(AvgWithdraw) + "% from " + str(len(data_list)) + " class(es) for " + orig_query + ": " + name
+    
 
-        for i in professorGrades:
-            
-            try:
-                professorGrades[i].extend(process_profQuery(i))
-                data = professorGrades[i]
-            except Exception as e:
-                print(e)
-            
-            if data[0] > bestProfAB:
-                bestProfName = i
-                bestProfAB = data[0]
-                bestProfLenCount = data[-1]
-            if data[1] > worstProfFW:
-                worstProfName = i
-                worstProfFW = data[1]
-                worstProfLenCount = data[-1]
+    bestProfName = "" #Professor name to go in professor string
+    bestProfAB = 0
+    bestProfLenCount = 0
+
+    worstProfName = ""
+    worstProfFW = 0
+    worstProfLenCount = 0
+
+    data = []
+
+    for i in professorGrades:
         
-        profString = "The statistically best professor is " + bestProfName + " with an A+B Avg of " + str(bestProfAB) + "% in " + str(bestProfLenCount) + " classes\n\nThe statistically worst professor is " + worstProfName + " with an F+W Avg of " + str(worstProfFW) + "% out of " + str(worstProfLenCount) + " class(es)" #Final professor string
+        try:
+            professorGrades[i].extend(process_profQuery(i))
+            data = professorGrades[i]
+        except Exception as e:
+            print(e)
+        
+        if data[0] > bestProfAB:
+            bestProfName = i
+            bestProfAB = data[0]
+            bestProfLenCount = data[-1]
+        if data[1] > worstProfFW:
+            worstProfName = i
+            worstProfFW = data[1]
+            worstProfLenCount = data[-1]
+    
+    profString = "The statistically best professor is " + bestProfName + " with an A+B Avg of " + str(bestProfAB) + "% in " + str(bestProfLenCount) + " classes\n\nThe statistically worst professor is " + worstProfName + " with an F+W Avg of " + str(worstProfFW) + "% out of " + str(worstProfLenCount) + " class(es)" #Final professor string
 
-        return courseString + "\n\n" + profString + "\n\n"
+    return courseString + "\n\n" + profString + "\n\n"
 
 
 
@@ -268,14 +271,29 @@ def getAllCourses():
         for i in range(len(temp)):
             f.write(temp[i]+"\n")
 
-initialize()
-getAllCourses()
+def go(course):
+    initialize()
+    getAllCourses()
+    searchCourse(course)
 
-while(True):
-    query = input("What course do you want to search? (Format: <Course>-<number> ie CPSC-3720)\n")
+def searchCourse(query):
+    # query = input("What course do you want to search? (Format: <Course>-<number> ie CPSC-3720)\n")
     try:
         
+        print("\n--------------------------------------------------------")
         print("Since Spring 2014, there was an average of " + process_Search(query))
+        print("DISCLAIMER:")
+        print("Not every professor listed will be at Clemson, this is a tool built for better information but not complete information")
+        print("Take it at your own discression")
+        print("\nIn addition, this system works on the Grade Distribution Releases located at https://www.clemson.edu/institutional-effectiveness/oir/data-reports/\n")
+        print("As a result, the limitations according to the GDR are as follows:")
+        print('*Course Sections that meet the following conditions are not included: Undergraduate classes with less than 10 students or Graduate classes with less than 5 students. In addition, if a section has all but 1 student making a common grade (example: All but one student makes a "B" in a class), the section is excluded.*')
+        print('\n----------------------------------------------------------------------------')
+    except NotADirectoryError as e:
+        print("Class not found, are you sure you used the correct format? (Ex: cpsc-2120)")
     except Exception as e:
+        
         print("Something went wrong somewhere, idk bout that")
         print(e)
+
+go('cpsc-2120')
