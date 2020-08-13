6a089e429a409cdce170d7bfa7d8bbb2de7cf0e6
Nathan-Binkley <nbink2@gmail.com>
2020-08-13 17:31:50 -0400

    Reddit Ready

--- Reddit.py
+++ Reddit.py
@@ -1,5 +1,11 @@
 import json
 import os
+import time
+
+# Reddit Stuff
+import praw
+import keys
+import praw.exceptions
 
 special_converted_files = ['2018Fall.csv','2018Spring.csv'] #these files have special formatting for names (ie ' "Smith, John William" ' is normal and in these it's simply 'Smith John William' without commas or quotes )
 #TBH, this is the easier way to parse but whatevs
@@ -11,6 +17,10 @@ master_prof_list = {}
 
 
 
+
+
+
+
 def process_Search(orig_query: str) -> str: #Primary search function and processing for a query. A query is generally defined by 
                                    #"Course-Number" of which it pulls the data from the DB.
     query = orig_query.upper()
@@ -18,27 +28,6 @@ def process_Search(orig_query: str) -> str: #Primary search function and process
     items = query.split("-")
     master_String = ''
 
-    # if("ALL" in items):
-    #     print("ALL IN QUERY")
-    #     print("HERE COMES THE SUN")
-    #     if items[0] == "ALL" and items[1] == "ALL":
-    #         print("ALLALL")
-    #         for i in master_list:
-    #             master_String += process_Search(i)
-    #     elif items[0] == "ALL":
-    #         for i in master_list:
-    #             if items[1] in i:
-    #                 master_String += process_Search(i)
-    #     elif items[1] == "ALL":
-    #         for i in master_list:
-    #             if items[0] in i:
-    #                 master_String += process_Search(i)
-    #     else:
-    #         print("Misunderstood")
-    #         return "HELP"
-    #     return master_String
-
-    # else:
     if query in master_list:
         data_list = master_list[query]
     else:
@@ -53,10 +42,8 @@ def process_Search(orig_query: str) -> str: #Primary search function and process
     W = []
     P = []
     NP = []
-    
 
     professorGrades = {}
-
     
     for i in data_list:
         A.append(i['A'])
@@ -66,11 +53,8 @@ def process_Search(orig_query: str) -> str: #Primary search function and process
         F.append(i['F'])
         W.append(i['W'])
 
-        
         searchableName = getFirstLast(i['Professor'])
-
         professorGrades[searchableName] = []
-        
 
     gradesList = (A,B,C,D,F,W,P,NP)
     
@@ -84,9 +68,8 @@ def process_Search(orig_query: str) -> str: #Primary search function and process
     AvgD = sum(D)//len(D)
     AvgF = sum(F)//len(F)
     AvgWithdraw = sum(W)//len(W)
-
-    courseString = "Average; A: " + str(AvgA) + "% B: " + str(AvgB) + "% C: " + str(AvgC) + "% D: " + str(AvgD) + "% F: " + str(AvgF) + "% W: " + str(AvgWithdraw) + "% from " + str(len(data_list)) + " class(es) for " + orig_query + ": " + name
     
+    courseString = 'Average; \nA: {}%\nB: {}%\nC: {}%\nD: {}%\nF: {}%\nW: {}%\nfrom {} class(es) for {}: {}'.format(AvgA, AvgB, AvgC, AvgD, AvgF, AvgWithdraw, len(data_list), orig_query, name)
 
     bestProfName = "" #Professor name to go in professor string
     bestProfAB = 0
@@ -107,19 +90,18 @@ def process_Search(orig_query: str) -> str: #Primary search function and process
             print(e)
         
         if data[0] > bestProfAB:
-            bestProfName = i
+            bestProfName = getInitials(i)
             bestProfAB = data[0]
             bestProfLenCount = data[-1]
         if data[1] > worstProfFW:
-            worstProfName = i
+            worstProfName = getInitials(i)
             worstProfFW = data[1]
             worstProfLenCount = data[-1]
     
-    profString = "The statistically best professor is " + bestProfName + " with an A+B Avg of " + str(bestProfAB) + "% in " + str(bestProfLenCount) + " classes\n\nThe statistically worst professor is " + worstProfName + " with an F+W Avg of " + str(worstProfFW) + "% out of " + str(worstProfLenCount) + " class(es)" #Final professor string
+    
+    profString = 'The statistically best professor has the initials {} with an A+B Avg of {}% in {} classes\n\nThe statistically worst professor has the initials {} with an F+W Avg of {}% out of {} class(es)'.format(bestProfName.replace('"', ''), bestProfAB, bestProfLenCount, worstProfName.replace('"', ''), worstProfFW, worstProfLenCount) #Final professor string
 
     return courseString + "\n\n" + profString + "\n\n"
-
-
 
 def getInitials(Name):
     initials = ""
@@ -133,16 +115,13 @@ def initialize():
     global master_list, master_prof_list
 
     if os.path.isfile('./master.json'): #SKIP PARSE DATA WHEN NOT NECESSARY
-        print("FOUND COURSE FILE")
         with open('master.json', 'r') as f:
             master_list = json.load(f)
         # print(master_list)
         if os.path.isfile('./master_prof.json'):
-            print("FOUND PROF FILE")
             with open('master_prof.json', 'r') as f:
                 master_prof_list = json.load(f)
-        else:
-            print("PROF FILE NOT FOUND")
+        
     else:
     
         with open("master.csv", "r") as f:
@@ -206,16 +185,13 @@ def initialize():
         with open("master.json","w+") as f: #Unformatted data, useful for space
             json.dump(master_list, f)
         with open("master_prof.json", "w+") as f:
-            json.dump(master_prof_list, f)
-          
+            json.dump(master_prof_list, f)      
 
 def getFirstLast(Name):
     FML = Name.split()
     First = FML[1]
     Last = FML[0]
     return First + " " + Last
-
-
 
 def process_profQuery(Name):
     data_list = master_prof_list[Name]
@@ -251,49 +227,72 @@ def process_profQuery(Name):
 
     return  [(AvgA + AvgB), (AvgF+AvgWithdraw), (len(data_list))]
 
-
-
-
 def toCamelCase(Name):
     items = Name.split()
     for i in range(len(items)):
         items[i] = items[i][0].upper() + items[i][1:].lower()
     return " ".join(items)
 
-def getAllCourses():
-    temp = []
-    for i in master_list:
-        if i[:-5] not in temp:
-            temp.append(i[:-5])
-        else:
-            pass
-    with open("CourseList.txt", "w") as f:
-        for i in range(len(temp)):
-            f.write(temp[i]+"\n")
-
 def go(course):
     initialize()
-    getAllCourses()
+    # getAllCourses()
     return searchCourse(course)
 
 def searchCourse(query):
-    # query = input("What course do you want to search? (Format: <Course>-<number> ie CPSC-3720)\n")
     try:
         string = ''
-        string += "\n--------------------------------------------------------"
+        string += "\n--------------------------------------------------------\n"
         string += "Since Spring 2014, there was an average of " + process_Search(query)
-        string += "DISCLAIMER:"
-        string += "Not every professor listed will be at Clemson, this is a tool built for better information but not complete information"
-        string += "Take it at your own discression"
-        string += "\nIn addition, this system works on the Grade Distribution Releases located at https://www.clemson.edu/institutional-effectiveness/oir/data-reports/\n"
-        string += "As a result, the limitations according to the GDR are as follows:"
-        string += '*Course Sections that meet the following conditions are not included: Undergraduate classes with less than 10 students or Graduate classes with less than 5 students. In addition, if a section has all but 1 student making a common grade (example: All but one student makes a "B" in a class), the section is excluded.*'
-        string += '\n----------------------------------------------------------------------------'
+        string += "\n\n^DISCLAIMER:\n"
+        string += "^(Not every professor listed will be at Clemson, this is a tool built for better information but not complete information)\n"
+        string += "^(Take it at your own discression)"
+        string += "\n^(In addition, this system works on the Grade Distribution Releases located at https://www.clemson.edu/institutional-effectiveness/oir/data-reports/)\n"
+        string += "^(As a result, the limitations according to the GDR are as follows:)\n"
+        string += '^(*Course Sections that meet the following conditions are not included: Undergraduate classes with less than 10 students or Graduate classes with less than 5 students. In addition, if a section has all but 1 student making a common grade `example: All but one student makes a "B" in a class`, the section is excluded.*)'
+        string += '\n\n^(Due to Reddit Policy, full names are not allowed -- Initials are substituted.)'
+        string += '\n\n^(This is meant to be an informative tool, not an end-all be all decision maker. Use your brain when using this app.)'
+        string += '\n\n----------------------------------------------------------------------------'
+    
     except NotADirectoryError as e:
-        print("Class not found, are you sure you used the correct format? (Ex: cpsc-2120)")
+        
+        return "Class not found, are you sure you used the correct format? (Ex: cpsc-2120)"
+        
     except Exception as e:
         
-        print("Something went wrong somewhere, idk bout that")
-        print(e)
+        return "Something went wrong somewhere, idk bout that"
+    
 
-go('cpsc-2120')
+    return string
+# Do reddit things
+def reddit():
+    reddit = praw.Reddit(client_id=keys.clientID,
+                     client_secret=keys.clientSecret,
+                     user_agent=keys.userAgent,
+                     username=keys.username,
+                     password=keys.password)
+
+    while True:
+        print("CONNECTED")
+        for mention in reddit.inbox.mentions(limit=25):
+            body = mention.body
+            course = body.split()[1]
+            try:
+                mention.reply(go(course))
+            except praw.exceptions.RedditAPIException as e:
+                print(e)
+                print("Sleeping for 10 minutes")
+                time.sleep(600)
+                mention.reply(go(course))
+            mention.mark_read()
+        time.sleep(300)
+
+def purge(count = 1000): #Just incase I need to purge the reddit account -- specifically after testing
+    mee = reddit.user.me()
+    i = 1
+    for c in mee.comments.new(limit=count):
+        c.edit("#")
+        c.delete()
+        print("Deleted " + str(i))
+        i += 1
+
+reddit()
